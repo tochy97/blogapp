a071af4f6729f0157954a3423f0b3080537bfbd9
Chytoe <tochyegeonu97@gmail.com>
2021-12-19 22:00:00 -0600

    fixed broken links

--- src/Dashboard/AboutSite.js
+++ src/Dashboard/AboutSite.js
@@ -9,7 +9,7 @@ const AboutSite = () => {
             <Row className="px-5 my-6 gap-5">
                 <Col lg={10}  className="mx-auto" style={{alignItems: "center"}}>
                     <Divider><h1>About Site</h1></Divider>
-                    <Card.Text>This is a site for those who want to post their creativity without the fear of unwanted outsiders. Each user must be created by an existing user. The owner of the site reserves the right to delete any accounts he wants. <b>No illegal content will be allowed.</b> To request an account, click <NavLink to="/aboutSite" style={{color:"black", textDecoration:''}}>here</NavLink> or the "Contact Me" link below. <br/>Thank you. - Tochy </Card.Text>
+                    <Card.Text>This is a site for those who want to post their creativity without the fear of unwanted outsiders. Each user must be created by an existing user. The owner of the site reserves the right to delete any accounts he wants. <b>No illegal content will be allowed.</b> To request an account, click <NavLink to="/req" style={{color:"black", textDecoration:''}}>here</NavLink> or the "Contact Me" link below. <br/>Thank you. - Tochy </Card.Text>
                 </Col>
             </Row>
         </Card>
