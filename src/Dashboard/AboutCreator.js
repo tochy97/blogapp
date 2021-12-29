fa9f0c1215f3bc2a90afbe7ec404dd9c8c326422
Chytoe <tochyegeonu97@gmail.com>
2021-12-28 22:02:54 -0600

    little updates

--- src/Dashboard/AboutCreator.js
+++ src/Dashboard/AboutCreator.js
@@ -8,7 +8,7 @@ const AboutCreator = () => {
         <Card className="py-4 text-center" style={{border:0}}>
             <Row className="px-5 my-6 gap-5">
                 <Col lg={10}  className="mx-auto" style={{alignItems: "center"}}>
-                    <Divider><h1>About Creator</h1></Divider>
+                    <Divider><h1>Welcome!!</h1></Divider>
                     <Card.Text>My name is Tochukwu Egeonu, or Tochy for short. I am a University of Texas at Arlington graduate of 2022, with my bachelor’s in computer science. I am originally from Nigeria, but I have lived in the US since the age of three. I was raised in the DFW area, specifically Grand Prairie, Texas. I spend my free time creating/updating little projects such as this one. If you like the look and feel of this site and want one of your own, feel free to contact me for a consultation. Feel free to check out my links below.</Card.Text>
                     <Card.Img src={me} alt="me" style={{width:"10rem", alignSelf: "center"}}></Card.Img>
                     <Card.Subtitle className='mt-2'>Phone: (469) - 658 - 6413 </Card.Subtitle>
